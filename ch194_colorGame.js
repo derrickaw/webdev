151f1e589843699965603b94bd3487f0ccf369d4
Derrick Williams <myutat@gmail.com>
2018-11-15 21:05:03 -0800

    added more click functionality per tutorial

--- ch194_colorGame.js
+++ ch194_colorGame.js
@@ -4,13 +4,16 @@ var colors = [
     "rgb(0, 255, 0)",
     "rgb(0, 255, 255)",
     "rgb(0, 0, 255)",
-    "rgb(255, 0 ,255)"
+    "rgb(255, 0, 255)"
 ]
 
 var squares = document.querySelectorAll(".square");
-var pickedColor = colors[3];
+var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
+var messageDisplay = document.querySelector("#message");
+
 colorDisplay.textContent = pickedColor;
+
 
 for(var i = 0; i < squares.length; i++) {
     // Add initial colors to squares
@@ -22,9 +25,24 @@ for(var i = 0; i < squares.length; i++) {
         var clickedColor = this.style.backgroundColor;
         // Compare color to pickedColor
         if(clickedColor === pickedColor) {
-            alert("Correct");
+            messageDisplay.textContent = "Correct!";
+            changeColors(clickedColor);
         } else {
-            alert("WRONG!!");
+            this.style.backgroundColor = "#232323";
+            messageDisplay.textContent = "Try again!";
         }
     });
+}
+
+function changeColors(color) {
+    // loop through all squares
+    for(var i = 0; i < squares.length; i++) {
+        // change each square color to matched given color
+        squares[i].style.backgroundColor = color;
+    }
+}
+
+function pickColor() {
+    var random = Math.floor(Math.random() * colors.length)
+    return colors[random];
 }
