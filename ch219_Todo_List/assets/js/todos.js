8813eded7cccef7bbb43aaea4d546f5fbe22a9ca
Derrick Williams <myutat@gmail.com>
2018-12-02 21:58:36 -0800

    added deleting capability

--- ch219_Todo_List/assets/js/todos.js
+++ ch219_Todo_List/assets/js/todos.js
@@ -1,6 +1,19 @@
 // Checkoff specific todos by clicking
 $("li").click(function(){
     $(this).toggleClass("completed");
+})
+
+// Click on X to delete todo
+$("span").click(function(event) {
+    $(this).parent().fadeOut(500, function() {
+        $(this).remove();
+    });
+    event.stopPropagation();
+})
+
+
+
+
 
     // // if li is gray; turn black
     // if($(this).css("color") === "rgb(128, 128, 128)") {
@@ -17,4 +30,3 @@ $("li").click(function(){
     //     });
     // }
 
-})
