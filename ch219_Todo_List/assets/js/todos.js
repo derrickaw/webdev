33b92f282c9fb72aa3b97830a16e68f45271be86
Derrick Williams <myutat@gmail.com>
2018-12-06 21:25:10 -0800

    added more functionality to todo list per tutorial

--- ch219_Todo_List/assets/js/todos.js
+++ ch219_Todo_List/assets/js/todos.js
@@ -1,16 +1,26 @@
 // Checkoff specific todos by clicking
-$("li").click(function(){
+$("ul").on("click", "li", function(){
     $(this).toggleClass("completed");
 })
 
 // Click on X to delete todo
-$("span").click(function(event) {
+$("ul").on("click", "span", function(event) {
     $(this).parent().fadeOut(500, function() {
         $(this).remove();
     });
     event.stopPropagation();
 })
 
+$("input[type='text']").keypress(function(event) {
+    // Check for enter key
+    if(event.which === 13) {
+        // grabbing new todo text from input
+        var todoText = $(this).val();
+        $(this).val("");
+        // create new li and add to ul
+        $("ul").append("<li><span>X</span> " + todoText + "</li>");
+    }
+})
 
 
 
